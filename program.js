7f6866345baf93c6542bb97169c68ad2c9c30826
Aleksander Chromik <achromik@gmail.com>
2017-09-26 23:37:27 +0200

    ADD - make modules

---
+++ program.js
@@ -0,0 +1,41 @@
+var OSinfo = require('./OSinfo');
+
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    var info = '    /node - node version\n\
+    /getOSinfo - system information\n\
+    /lang - OS language\n\
+    /help - this info\n\
+    /exit - exit program\n';
+
+    //console.log(process.env);
+    if( input !== null) {
+        var instruction = input.toString().trim();
+        switch (instruction) {
+            case '/exit' : 
+                process.stdout.write('Quiting app!\n');
+                process.exit();
+                break;
+            case '/lang' :
+                process.stdout.write('Environment language: ' + process.env.lang + '\n');    
+                break;
+            case '/node' :
+                process.stdout.write('Node version: ' + process.versions.node + '\n');
+                break; 
+            case '/help' :
+                process.stdout.write('Usage: \n' + info);
+                break;    
+            case '/getOSinfo':
+                OSinfo.print();                
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n\n' + info);
+                                      
+        }
+
+    }
+});
+
+
+
